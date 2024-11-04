d687fc68936b39075fd811e9e2324558690c8d32
yyu26 <yyu26@slb.com>
2024-11-04 18:46:22 +0800

    add new service and test

---
+++ TodoItem.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TodoItem.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TodoItems.Core/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+using TodoItems.Core.Model;
+
+namespace TodoItems.Core;
+
+public class TodoItem
+{
+    public string GetId()
+    {
+        return "1";
+    }
+
+}
