e35619a4af0aedd456259277ea73654d1c5081ed
yyu26 <yyu26@slb.com>
2024-11-04 18:53:21 +0800

    feat: add new V2 Controller

---
+++ ToDoItemList/Controllers/TodoListItemsV2Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoItem.core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoListItemsV2Controller : ControllerBase
+    {
+    }
+}
